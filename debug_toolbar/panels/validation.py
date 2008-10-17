cf6954bf9e8a5d52b6537c63f3d20b764a61f6cc
Adam Gomaa <adam@adam.gomaa.us>
2008-10-17 09:24:38 -0400

    Add a .to_data to ValidationPanel and add some more effecient properties for .errors and .warnings.

--- debug_toolbar/panels/validation.py
+++ debug_toolbar/panels/validation.py
@@ -12,19 +12,33 @@ class ValidationPanel(DebugPanel):
     has_content = True
 
     def title(self):
-        warnings = [err for err in self.tidydoc.errors if err.severity == 'W']
-        errors = [err for err in self.tidydoc.errors if err.severity == 'E']
-        return "%s errors/%s warnings" % (len(errors), len(warnings))
+        return "%s errors/%s warnings" % (len(self.errors), len(self.warnings))
 
     def url(self):
         return ''
 
     def content(self):
-        warnings = [err for err in self.tidydoc.errors if err.severity == 'W']
-        errors = [err for err in self.tidydoc.errors if err.severity == 'E']
-        context = {"warnings": warnings, "errors": errors}
+        context = {"warnings": self.warnings, "errors": self.errors}
         return render_to_string('debug_toolbar/panels/validation.html', context)
+
+    @property
+    def warnings(self):
+        if not hasattr(self, "_warnings"):
+            self._warnings = [err for err in self.tidydoc.errors
+                              if err.severity == 'W']
+        return self._warnings
+
+    @property
+    def errors(self):
+        if not hasattr(self, "_errors"):
+            self._errors = [err for err in self.tidydoc.errors
+                              if err.severity == 'E']
+        return self._errors
 
     def process_response(self, request, response):
         import tidy
         self.tidydoc = tidy.parseString(response.content)
+
+    def to_data(self):
+        return {"errors": self.errors,
+                "warnings": self.warnings}
